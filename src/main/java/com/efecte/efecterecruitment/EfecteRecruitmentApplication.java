0ddef4e681545ed407efc3f0f17c2a2c96be4310
mateuszpomykalski <mateusz.pomykalski@multilot.com>
2024-09-27 09:38:00 +0200

    [#1] - create basic spring-boot project structure with swagger configured

---
+++ src/main/java/com/efecte/efecterecruitment/EfecteRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.efecte.efecterecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfecteRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfecteRecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/efecte/efecterecruitment/EfecteRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efecte.efecterecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfecteRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
