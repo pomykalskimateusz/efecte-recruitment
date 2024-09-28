f46b34ccf00787f8d7aaf97c06f5070b8ba38468
mateuszpomykalski <mateusz.pomykalski@multilot.com>
2024-09-28 09:07:44 +0200

    [#3] - add tests for updating post

--- src/test/java/com/efecte/efecterecruitment/post/PostServiceTest.java
+++ src/test/java/com/efecte/efecterecruitment/post/PostServiceTest.java
@@ -1,8 +1,10 @@
 package com.efecte.efecterecruitment.post;
 
 import com.efecte.efecterecruitment.DatabaseContainer;
+import com.efecte.efecterecruitment.exception.ConflictException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +26,7 @@ public class PostServiceTest extends DatabaseContainer {
     @ParameterizedTest()
     @ValueSource(ints = {100, 150, 200})
     void shouldCreatePostWithValidContentLength(int contentLength) {
-        // GIVEN mocked session account id and post content
+        // GIVEN mocked session account id and post content with valid length
         var sessionAccountId = 1L;
         var content = generatePostContent(contentLength);
 
@@ -42,12 +44,67 @@ public class PostServiceTest extends DatabaseContainer {
     @ParameterizedTest()
     @ValueSource(ints = {201, 250})
     void shouldThrowExceptionForInvalidContentLengthDuringSavingPost(int contentLength) {
-        // GIVEN mocked session account id and post content with length 250
+        // GIVEN mocked session account id and post content with invalid length
         var sessionAccountId = 1L;
         var content = generatePostContent(contentLength);
 
         // WHEN saving post with invalid content length THEN exception should be thrown
         assertThrows(IllegalArgumentException.class, () -> postService.savePost(sessionAccountId, content));
+    }
+
+    @ParameterizedTest()
+    @ValueSource(ints = {100, 150, 200})
+    void shouldUpdatePostWithValidContentLengthAndVersion(int contentLength) {
+        // GIVEN mocked session account id and created post
+        var sessionAccountId = 1L;
+        var postId = createPost(sessionAccountId);
+
+        // WHEN updating post with valid content and correct version (equals 1)
+        var content = generatePostContent(contentLength);
+        postService.updatePost(sessionAccountId, postId, content, 1);
+
+        // THEN post content should be updated along with version
+        var updatedPost = postService.fetchPost(sessionAccountId, postId);
+        assertEquals(postId, updatedPost.getId());
+        assertEquals(content, updatedPost.getContent());
+        assertEquals(2, updatedPost.getVersion());
+    }
+
+    @ParameterizedTest()
+    @ValueSource(ints = {201, 250})
+    void shouldThrowExceptionForInvalidContentLengthDuringUpdatingPost(int contentLength) {
+        // GIVEN mocked session account id and created post
+        var sessionAccountId = 1L;
+        var postId = createPost(sessionAccountId);
+
+        // WHEN updating post with invalid content THEN exception should be thrown
+        var content = generatePostContent(contentLength);
+        assertThrows(IllegalArgumentException.class, () -> postService.updatePost(sessionAccountId, postId, content, 1));
+    }
+
+    @Test
+    void shouldThrowExceptionForOutdatedVersionDuringUpdatingPost() {
+        // GIVEN mocked session account id and updated post (after update version equals 2)
+        var sessionAccountId = 1L;
+        var postId = createPost(sessionAccountId);
+        postService.updatePost(sessionAccountId, postId, generatePostContent(10), 1);
+
+        // WHEN updating post with outdated version THEN conflict exception should be thrown
+        assertThrows(ConflictException.class, () -> postService.updatePost(sessionAccountId, postId, generatePostContent(20), 1));
+    }
+
+    @Test
+    void shouldThrowExceptionForUpdatingPostByNotOwner() {
+        // GIVEN mocked session account id and created post
+        var sessionAccountId = 1L;
+        var postId = createPost(sessionAccountId);
+
+        // WHEN updating post with differ account id than mocked session account id THEN exception should be thrown
+        assertThrows(IllegalArgumentException.class, () -> postService.updatePost(2L, postId, generatePostContent(10), 1));
+    }
+
+    private UUID createPost(Long accountId) {
+        return postService.savePost(accountId, generatePostContent(100));
     }
 
     private String generatePostContent(int length) {
