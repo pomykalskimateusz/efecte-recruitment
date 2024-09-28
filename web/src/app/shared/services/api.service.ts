d3084c3f95f69fff43b70cbd4cb87f72907ee731
pomykalskimateusz <pomykalskimateusz@gmail.com>
2024-09-28 23:01:52 +0200

    #9 - extract missing methods to api-service, fix DI (#8)
    Co-authored-by: mateuszpomykalski <mateusz.pomykalski@multilot.com>

--- web/src/app/shared/services/api.service.ts
+++ web/src/app/shared/services/api.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Post} from "../../models/post.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,11 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {}
 
   savePost(content: string, handleSuccess: () => void, handleError: (errorMessage: string) => void) {
-    this.httpClient.post(`http://localhost:8080/posts`, {content: content}).subscribe({
-      next: (_) => handleSuccess(),
-      error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
+    this.httpClient
+      .post(`http://localhost:8080/posts`, {content: content})
+      .subscribe({
+        next: (_) => handleSuccess(),
+        error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
     })
   }
 
@@ -19,17 +22,33 @@ export class ApiService {
       content: content,
       version: version
     }
-    this.httpClient.put(`http://localhost:8080/posts/${postId}`, request).subscribe({
-      next: (_) => handleSuccess(),
-      error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
+    this.httpClient
+      .put(`http://localhost:8080/posts/${postId}`, request)
+      .subscribe({
+        next: (_) => handleSuccess(),
+        error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
     })
   }
 
   deletePost(postId: string, postVersion: number, handleSuccess: () => void, handleError: (errorMessage: string) => void) {
-    this.httpClient.delete(`http://localhost:8080/posts/${postId}?version=${postVersion}`).subscribe({
-      next: (_) => handleSuccess(),
-      error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
+    this.httpClient
+      .delete(`http://localhost:8080/posts/${postId}?version=${postVersion}`)
+      .subscribe({
+        next: (_) => handleSuccess(),
+        error: (error: HttpErrorResponse) => handleError(this.buildErrorMessage(error))
     })
+  }
+
+  fetchPost(postId: string, handleResponse: (post: Post) => void) {
+    this.httpClient
+      .get<Post>(`http://localhost:8080/posts/${postId}`)
+      .subscribe(data => handleResponse(data))
+  }
+
+  fetchPostList(handleResponse: (postList: Post[]) => void) {
+    this.httpClient
+      .get<Post[]>(`http://localhost:8080/posts`)
+      .subscribe((data) => handleResponse(data))
   }
 
   buildErrorMessage = (error: HttpErrorResponse) => {
